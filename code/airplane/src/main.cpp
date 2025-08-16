00b3d5ff17cdf851d3bef2416ba931ec3c69438d
Breninho <brenosoares4303@gmail.com>
2025-08-16 00:54:47 -0300

    Aysha linda do meu coração, ajudou no tcc

--- code/airplane/src/main.cpp
+++ code/airplane/src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PIN_STROBE 27
+#define PIN_NAV 14
+
+unsigned long strobe_millis = 0;
+int step = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(PIN_STROBE, OUTPUT);
+  pinMode(PIN_NAV, OUTPUT);
+
+  digitalWrite(PIN_NAV, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  unsigned long current_millis = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(step == 0 && current_millis - strobe_millis >= 1000)
+  {
+    digitalWrite(PIN_STROBE, HIGH);
+    strobe_millis = current_millis;
+    step++;
+  }
+
+  if(step == 1 && current_millis - strobe_millis >= 50)
+  {
+    digitalWrite(PIN_STROBE, LOW);
+    strobe_millis = current_millis;
+    step++;
+  }
+
+  if(step == 2 && current_millis - strobe_millis >= 75)
+  {
+    digitalWrite(PIN_STROBE, HIGH);
+    strobe_millis = current_millis;
+    step++;
+  }
+
+  if(step == 3 && current_millis - strobe_millis >= 50)
+  {
+    digitalWrite(PIN_STROBE, LOW);
+    strobe_millis = current_millis;
+    step = 0;
+  }
 }
